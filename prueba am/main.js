a7813771c60dec58367187860859973d6d5be118
Andrey-Ossa <ossauver@gmail.com>
2026-05-12 10:53:51 -0500

    Add files via upload

---
+++ prueba am/main.js
@@ -0,0 +1,14 @@
+let mensaje = "estas lista";
+alert (mensaje);
+let mensaje2 = "seguraa";
+alert (mensaje2);
+let mensaje3 = "bueno,";
+alert (mensaje3);
+let mensaje4 = "ponte audifonos o subele al volumen";
+alert (mensaje4);
+let mensaje5 = "perdona lo poco";
+alert (Mensaje5)
+let mensaje6 = "es con mucho amor, espero te guste "
+alert (mensaje6)
+let mensaje7 = "🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰🥰"
+alert (mensaje7)
